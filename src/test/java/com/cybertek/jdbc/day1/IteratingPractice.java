06e4e7b51ce6cea7c98372a132d24e2dfa1611ab
aicetin <icetin35@gmail.com>
2020-08-03 16:38:21 -0500

    some added

--- src/test/java/com/cybertek/jdbc/day1/IteratingPractice.java
+++ src/test/java/com/cybertek/jdbc/day1/IteratingPractice.java
@@ -6,44 +6,27 @@ public class IteratingPractice {
     public static void main(String[] args) throws SQLException {
         String connectionStr = "jdbc:oracle:thin:@34.229.39.31:1521:XE";
 
-        String username = "hr";
-        String password = "hr";
-        Connection conn = DriverManager.getConnection(connectionStr, username, password);
+        String username = "hr" ;
+        String password = "hr" ;
+        Connection conn = DriverManager.getConnection(connectionStr,username,password) ;
         Statement stmnt = conn.createStatement();
-        ResultSet rs = stmnt.executeQuery("SELECT * FROM REGIONS");
+        //ResultSet rs   =   stmnt.executeQuery("SELECT * FROM COUNTRIES WHERE REGION_ID = 1") ;
+        ResultSet rs   =   stmnt.executeQuery("SELECT * FROM COUNTRIES") ;
 
         // as long as rs.next() return true I know I have next row to print the data
         // we will keep looping as long as rs.next() return true
+        while(rs.next()==true){
 
-        while (rs.next() == true ){   //while ( rs.next() ){
-
-            System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
+            System.out.println(rs.getString(1)+ " "
+                    + rs.getString(2) + " "
+                    + rs.getInt(3) ); /// this is getting region id as number instead of String
 
         }
-
-        // TASK 1 :
-        //  CREATE A NEW CLASS , ADD CONNECTION , STATEMENT , RESULTSET
-        // AND TRY TO PRINT OUT EACH AND EVERYTHING UNDER COUNTRIES TABLE
-
-
-
-//
-//        rs.next(); // currently we are at the first row
-//        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
-//
-//        rs.next();// currently we are at the second row
-//        System.out.println( rs.getString(1) + " " + rs.getString(2) );
-//
-//        rs.next(); // currently we are at the third row
-//        System.out.println( rs.getString("REGION_ID") + " " + rs.getString("REGION_NAME") );
-//
-////        rs.next();// currently we are at the forth row
-////        System.out.println( rs.getString(1) + " " + rs.getString(2) );
-//
-//        System.out.println(  rs.next()   );         ;// currently we are at the forth row
-//        //System.out.println( rs.getString(1) + " " + rs.getString(2) );
-
-
+//        rs.previous();
+//------ cleaning up -----
+        rs.close();
+        stmnt.close();
+        conn.close();
 
 
     }
